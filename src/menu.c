031772eae95a3d48f911220dea7510fa7ba694d1
eric-albuquer <ega2@cesar.school>
2025-05-13 17:26:51 -0300

    Deixando o layout do menu mais flexivel

--- src/menu.c
+++ src/menu.c
@@ -5,59 +5,70 @@
 
 static const int width  = 200;
 static const int height = 50;
+static const int padding = 50; // Espaçamento entre os botões
 
-static Button buttons[] = {
-    (Button) {"1 JOGADOR", (Vector2) {0, 0}, 0},
-    (Button) {"2 JOGADORES", (Vector2) {0, 0}, 0},
+static const int textBoxHeight = 20;
+
+static char* textContent = "FORMULA C";
+static int fontSize = 30;
+
+static Button BUTTONS[] = {
+    {"1 JOGADOR", {0, 0}, 0},
+    {"2 JOGADORES", {0, 0}, 0},
 };
 
-static const int buttonsLen = sizeof(buttons) / sizeof(Button);
+static const int buttonsLen = sizeof(BUTTONS) / sizeof(Button);
+
+static Vector2 textBox = {0, 0};
 
 void setup_menu() {
+    int dy = padding + height;
+    int y = (SCREEN_HEIGHT - dy * (buttonsLen - 1) + padding) / 2;
     for (int i = 0; i < buttonsLen; i++) {
-        buttons[i].pos.y = (SCREEN_HEIGHT + (i * height * 2.5f)) / 2.0f;
-        buttons[i].pos.x = (SCREEN_WIDTH - width) / 2.0f;
+        BUTTONS[i].pos.y = y;
+        BUTTONS[i].pos.x = (SCREEN_WIDTH - width) / 2.0f;
+        y += dy;
     }
+    
+    textBox.x = (SCREEN_WIDTH - MeasureText(textContent, fontSize)) / 2;
+    textBox.y = (SCREEN_HEIGHT - dy * buttonsLen + textBoxHeight) / 2;
 }
 
 void update_menu() {
     Vector2 mouse = GetMousePosition();
 
     if (CheckCollisionPointRec(mouse,
-                               (Rectangle) {buttons[0].pos.x, buttons[0].pos.y, width, height})) {
-        buttons[0].hovered = 1;
+                               (Rectangle) {BUTTONS[0].pos.x, BUTTONS[0].pos.y, width, height})) {
+        BUTTONS[0].hovered = 1;
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             setup_game(SINGLEPLAYER);
             state.screen = GAME;
         }
     } else {
-        buttons[0].hovered = 0;
+        BUTTONS[0].hovered = 0;
     }
 
     if (CheckCollisionPointRec(mouse,
-                               (Rectangle) {buttons[0].pos.x, buttons[0].pos.y, width, height})) {
-        buttons[0].hovered = 1;
+                               (Rectangle) {BUTTONS[1].pos.x, BUTTONS[1].pos.y, width, height})) {
+        BUTTONS[1].hovered = 1;
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             setup_game(SINGLEPLAYER);
             state.screen = GAME;
         }
     } else {
-        buttons[0].hovered = 0;
+        BUTTONS[1].hovered = 0;
     }
 }
 
 void draw_menu() {
     ClearBackground(BLACK);
-
-    char *text = "FORMULA C";
-    DrawText(text, (SCREEN_WIDTH - MeasureText(text, 20)) / 2.0f,
-             (SCREEN_HEIGHT / 2.0f) - (height * 2), 20, WHITE);
+    DrawText(textContent, textBox.x, textBox.y, fontSize, PURPLE);
 
     for (int i = 0; i < buttonsLen; i++) {
-        Button    btn  = buttons[i];
+        Button    btn  = BUTTONS[i];
         Rectangle rect = (Rectangle) {btn.pos.x, btn.pos.y, width, height};
         DrawRectangleRec(rect, btn.hovered ? GOLD : WHITE);
-        DrawText(btn.text, (rect.x + ((rect.width - MeasureText(btn.text, 20)) / 2.0f)),
-                 (rect.y + ((rect.height - 20) / 2.0f)), 20, BLACK);
+        DrawText(btn.text, (rect.x + ((rect.width - MeasureText(btn.text, 20)) / 2)),
+                 (rect.y + ((rect.height - textBoxHeight) / 2)), textBoxHeight, BLACK);
     }
 }
