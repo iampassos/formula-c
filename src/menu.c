3ea4468bbd5192bc132322c2108678f1aeaf130f
iampassos <78429887+iampassos@users.noreply.github.com>
2025-05-13 17:41:56 -0300

    Logic improvement

--- src/menu.c
+++ src/menu.c
@@ -3,14 +3,14 @@
 #include "game.h"
 #include <raylib.h>
 
-static const int width  = 200;
-static const int height = 50;
-static const int padding = 50; // Espaçamento entre os botões
+static const int width   = 200;
+static const int height  = 50;
+static const int padding = 25; // Espaçamento entre os botões
 
 static const int textBoxHeight = 20;
 
-static char* textContent = "FORMULA C";
-static int fontSize = 30;
+static char *textContent = "FORMULA C";
+static int   fontSize    = 30;
 
 static Button BUTTONS[] = {
     {"1 JOGADOR", {0, 0}, 0},
@@ -23,46 +23,38 @@ static Vector2 textBox = {0, 0};
 
 void setup_menu() {
     int dy = padding + height;
-    int y = (SCREEN_HEIGHT - dy * (buttonsLen - 1) + padding) / 2;
+    int y  = (SCREEN_HEIGHT - dy * (buttonsLen - 1) + padding) / 2;
     for (int i = 0; i < buttonsLen; i++) {
         BUTTONS[i].pos.y = y;
         BUTTONS[i].pos.x = (SCREEN_WIDTH - width) / 2.0f;
         y += dy;
     }
-    
-    textBox.x = (SCREEN_WIDTH - MeasureText(textContent, fontSize)) / 2;
-    textBox.y = (SCREEN_HEIGHT - dy * buttonsLen + textBoxHeight) / 2;
+
+    textBox.x = (SCREEN_WIDTH - MeasureText(textContent, fontSize)) / 2.0f;
+    textBox.y = (SCREEN_HEIGHT - dy * buttonsLen + textBoxHeight) / 2.0f;
 }
 
 void update_menu() {
     Vector2 mouse = GetMousePosition();
 
-    if (CheckCollisionPointRec(mouse,
-                               (Rectangle) {BUTTONS[0].pos.x, BUTTONS[0].pos.y, width, height})) {
-        BUTTONS[0].hovered = 1;
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            setup_game(SINGLEPLAYER);
-            state.screen = GAME;
-        }
-    } else {
-        BUTTONS[0].hovered = 0;
+    BUTTONS[0].hovered = CheckCollisionPointRec(
+        mouse, (Rectangle) {BUTTONS[0].pos.x, BUTTONS[0].pos.y, width, height});
+
+    if (BUTTONS[0].hovered && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        setup_game(SINGLEPLAYER);
+        state.screen = GAME;
     }
 
-    if (CheckCollisionPointRec(mouse,
-                               (Rectangle) {BUTTONS[1].pos.x, BUTTONS[1].pos.y, width, height})) {
-        BUTTONS[1].hovered = 1;
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            setup_game(SINGLEPLAYER);
-            state.screen = GAME;
-        }
-    } else {
-        BUTTONS[1].hovered = 0;
+    BUTTONS[1].hovered = CheckCollisionPointRec(
+        mouse, (Rectangle) {BUTTONS[1].pos.x, BUTTONS[1].pos.y, width, height});
+
+    if (BUTTONS[1].hovered && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
     }
 }
 
 void draw_menu() {
     ClearBackground(BLACK);
-    DrawText(textContent, textBox.x, textBox.y, fontSize, PURPLE);
+    DrawText(textContent, textBox.x, textBox.y, fontSize, RED);
 
     for (int i = 0; i < buttonsLen; i++) {
         Button    btn  = BUTTONS[i];
